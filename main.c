ef5a0822353b82b52ac4e3264c310a9092ce4eec
V4g480nd <sub_0wolf@inorbit.com>
2016-02-26 12:34:00 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #define NOTHING_MORE_NOTHING_LESS 1
-
+#define SOMETHING !NOTHING_MORE_NOTHING_LESS
 int main()
 {
   if (NOTHING_MORE_NOTHING_LESS)
     printf ("Nothing is here\n");
+  else if (SOMETHING)
+    printf ("You are something else\n")
   return 0
 }
